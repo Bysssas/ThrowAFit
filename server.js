9daffb414d823fcd44a3681cbf895c4adf69b459
B yss <20240031277@my.xu.edu.ph>
2025-11-01 17:59:33 +0800

    upload update

--- server.js
+++ server.js
@@ -13,10 +13,9 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use("/api/upload", uploadRoutes);
 
 // Routes
 app.use("/api/items", itemRoutes);
-
+app.use("/api/upload", uploadRoutes);
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
